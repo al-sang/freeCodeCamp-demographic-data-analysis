da9d1a4db4804fefa2115f22caf4afbebc15687e
Alex <u7132739@anu.edu.au>
2021-09-08 21:12:01 +1000

    3 Questions remaining

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,35 +3,41 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv('adult.data.csv')
+    total = df.shape[0]
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df.race.value_counts()
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = df.loc[df['sex'] == 'Male'].age.mean()
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    percentage_bachelors = (
+        df.loc[df['education'] == 'Bachelors'].shape[0] / total) * 100
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    high_ed_labels = ('Bachelors', 'Masters', 'Doctorate')
+    higher_education = df.query('education in @high_ed_labels')
+    lower_education = df.query('education not in @high_ed_labels')
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    high = '>50K'
+    higher_education_rich = higher_education.query('salary == @high')
+    lower_education_rich = lower_education.query('salary == @high')
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    min_workers = df.loc[df['hours-per-week'] == min_work_hours]
+    num_min_workers = min_workers.shape[0]
 
-    rich_percentage = None
+    rich_percentage = (min_workers.query(
+        'salary == @high').shape[0] / num_min_workers) * 100
 
     # What country has the highest percentage of people that earn >50K?
     highest_earning_country = None
@@ -43,15 +49,19 @@ def calculate_demographic_data(print_data=True):
     # DO NOT MODIFY BELOW THIS LINE
 
     if print_data:
-        print("Number of each race:\n", race_count) 
+        print("Number of each race:\n", race_count)
         print("Average age of men:", average_age_men)
         print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
-        print(f"Percentage with higher education that earn >50K: {higher_education_rich}%")
-        print(f"Percentage without higher education that earn >50K: {lower_education_rich}%")
+        print(
+            f"Percentage with higher education that earn >50K: {higher_education_rich}%")
+        print(
+            f"Percentage without higher education that earn >50K: {lower_education_rich}%")
         print(f"Min work time: {min_work_hours} hours/week")
-        print(f"Percentage of rich among those who work fewest hours: {rich_percentage}%")
+        print(
+            f"Percentage of rich among those who work fewest hours: {rich_percentage}%")
         print("Country with highest percentage of rich:", highest_earning_country)
-        print(f"Highest percentage of rich people in country: {highest_earning_country_percentage}%")
+        print(
+            f"Highest percentage of rich people in country: {highest_earning_country_percentage}%")
         print("Top occupations in India:", top_IN_occupation)
 
     return {
