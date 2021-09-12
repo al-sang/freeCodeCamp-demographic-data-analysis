bdeea8d5e89df35a7ce91b042532ca935cb7436b
Alex <u7132739@anu.edu.au>
2021-09-12 16:39:43 +1000

    Completed deomgraphic - messy

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -10,24 +10,30 @@ def calculate_demographic_data(print_data=True):
     race_count = df.race.value_counts()
 
     # What is the average age of men?
-    average_age_men = df.loc[df['sex'] == 'Male'].age.mean()
+    average_age_men = round(df.loc[df['sex'] == 'Male'].age.mean(), 1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = (
-        df.loc[df['education'] == 'Bachelors'].shape[0] / total) * 100
+    percentage_bachelors = round((
+        df.loc[df['education'] == 'Bachelors'].shape[0] / total) * 100, 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
     high_ed_labels = ('Bachelors', 'Masters', 'Doctorate')
+
     higher_education = df.query('education in @high_ed_labels')
     lower_education = df.query('education not in @high_ed_labels')
 
     # percentage with salary >50K
     high = '>50K'
-    higher_education_rich = higher_education.query('salary == @high')
-    lower_education_rich = lower_education.query('salary == @high')
+
+    higher_education_rich = round((higher_education.query(
+        'salary == @high').shape[0] / higher_education.shape[0]) * 100, 1)
+    lower_education_rich = round((lower_education.query(
+        'salary == @high').shape[0] / lower_education.shape[0]) * 100, 1)
+    round(higher_education_rich, 1)
+    round(lower_education_rich, 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
     min_work_hours = df['hours-per-week'].min()
@@ -40,13 +46,24 @@ def calculate_demographic_data(print_data=True):
         'salary == @high').shape[0] / num_min_workers) * 100
 
     # What country has the highest percentage of people that earn >50K?
-    highest_earning_country = None
-    highest_earning_country_percentage = None
+    rich_count = df.query(
+        'salary == @high')[['native-country', 'salary']].groupby('native-country').count()
+
+    percentages = pd.concat(
+        [rich_count, df['native-country'].value_counts()], axis=1)
+    percentages.dropna(inplace=True)
+    percentages['rich_percentage'] = (
+        percentages['salary']/percentages['native-country'])*100
+    percentages.sort_values(by='rich_percentage',
+                            ascending=False, inplace=True)
+
+    highest_earning_country = percentages.iloc[0].name
+    highest_earning_country_percentage = round(percentages.iloc[0][2], 1)
 
     # Identify the most popular occupation for those who earn >50K in India.
-    top_IN_occupation = None
+    india_rich = df[(df['native-country'] == 'India') & (df['salary'] == high)]
 
-    # DO NOT MODIFY BELOW THIS LINE
+    top_IN_occupation = india_rich['occupation'].value_counts().index[0]
 
     if print_data:
         print("Number of each race:\n", race_count)
